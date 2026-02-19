cc805aba4a64f0a47bd6f543ea61e4370cda1f7d
JainDikshaa <dj5351@srmist.edu.in>
2026-02-19 11:15:57 +0530

    UC5 - Palindrome using Stack

--- PalindromeCheckerApp/src/Main.java
+++ PalindromeCheckerApp/src/Main.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
 import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word: ");
         String input = sc.nextLine();
-
         Stack<Character> stack = new Stack<>();
-
         for(char c : input.toCharArray())
             stack.push(c);
-
         String reversed = "";
         while(!stack.isEmpty())
             reversed += stack.pop();
-
         if(input.equals(reversed))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
-
         sc.close();
     }
 }
