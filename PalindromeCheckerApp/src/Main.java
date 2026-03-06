581151c6dc22d2da733ed57fc57193f30274f4b3
JainDikshaa <dj5351@srmist.edu.in>
2026-03-06 09:28:12 +0530

    UC13 - Performance comparison of palindrome algorithms

--- PalindromeCheckerApp/src/Main.java
+++ PalindromeCheckerApp/src/Main.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 public class Main {
 
-    public static boolean isPalindrome(String input) {
+    // Method 1: StringBuilder reverse
+    public static boolean checkPalindromeReverse(String input) {
+        String reversed = new StringBuilder(input).reverse().toString();
+        return input.equals(reversed);
+    }
 
-        input = input.replaceAll("\\s+", "").toLowerCase();
-
+    // Method 2: Two-pointer method
+    public static boolean checkPalindromeTwoPointer(String input) {
         int left = 0;
         int right = input.length() - 1;
 
@@ -16,21 +20,30 @@ public class Main {
             left++;
             right--;
         }
-
         return true;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter text: ");
+        System.out.print("Enter word: ");
         String input = sc.nextLine();
 
-        if(isPalindrome(input))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
+        // Method 1 timing
+        long start1 = System.nanoTime();
+        boolean result1 = checkPalindromeReverse(input);
+        long end1 = System.nanoTime();
+
+        // Method 2 timing
+        long start2 = System.nanoTime();
+        boolean result2 = checkPalindromeTwoPointer(input);
+        long end2 = System.nanoTime();
+
+        System.out.println("Reverse Method: " + result1);
+        System.out.println("Time taken: " + (end1 - start1) + " ns");
+
+        System.out.println("Two Pointer Method: " + result2);
+        System.out.println("Time taken: " + (end2 - start2) + " ns");
 
         sc.close();
     }
